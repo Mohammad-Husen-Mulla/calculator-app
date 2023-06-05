cca109207ee983fcac083dfcc61441813e7c8e1c
Mohammad-Husen-Mulla <mohammadmulla29@gmail.com>
2023-06-05 20:40:59 +0530

    Modified the logic

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
-console.log(document.querySelector('.num-1'));
-document.querySelector('.divide').addEventListener('click', function () {
+let plus = document.querySelector('.plus');
+let minus = document.querySelector('.multiply');
+let euqal = document.querySelector('.equal');
+let input = "";
+let res = 0;
+let arrayResult = [];
 
-  console.log("clicked");
+function displayKeys(value) {
+  input = input + String(value);
+  document.querySelector('.display-calc').textContent = input;
+}
+
+function clearNumbers() {
+  input = "";
+  document.querySelector('.display-calc').textContent = "";
+}
+
+const addition = function (result, number) {
+  return Number(result) + Number(number);
+}
+
+
+document.querySelectorAll('.number').forEach(num => num.addEventListener('click', function () {
+
+  if (input.length <= 45) {
+    displayKeys(this.value);
+
+
+
+  }
+
+  arrayResult.push(this.value);
+  console.log(arrayResult);
+
+}));
+
+euqal.addEventListener('click', function () {
+
+
 });
+
+function calculation(signs) {
+
+  switch (signs) {
+    case '+': {
+      addition(result,)
+      break;
+    }
+  }
+}
+
